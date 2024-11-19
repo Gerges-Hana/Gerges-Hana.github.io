4524828e45ed4787d30dbe0fd600a3f92f0de7ef
gerges <gerges.hana@x-era.ae>
2024-11-19 14:43:55 +0200

    gerges hana cv

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*<!-- ================Typing Animation================== -->*/
+ var typed =new Typed(".typing",{
+    strings:["","Software Engineer","Back-End Developer"],
+    typeSpeed:70,
+    BackSpeed:60,
+    loop:true
+})
